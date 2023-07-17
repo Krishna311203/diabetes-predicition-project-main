3efa5c5352988c74eed136c1ac1acda7a279534a
Krishna311203 <krishnashrivastava84@gmail.com>
2023-07-17 08:59:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from './Navbar'
+import Form from './Form'
+function App() {
+ 
+  return (
+    <>
+       <Navbar></Navbar>
+       <Form></Form>
+    </>
+  )
+}
+
+export default App
